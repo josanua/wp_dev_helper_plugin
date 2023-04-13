52cafcbf489cc9ba8a3ab768f663372b47afc90a
josanua <thrueword@gmail.com>
2023-04-13 14:08:58 +0300

    Added some structure ideeas.

--- dev-helper-plugin.php
+++ dev-helper-plugin.php
@@ -22,14 +22,22 @@ if (!defined('WPINC')) {
     die;
 }
 
-define('PLUGIN_NAME','DEV_HELPER_PLUGIN');
-
 /**
  * Currently plugin version.
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PLUGIN_VERSION', '1.0.0' );
+if (!defined('DHP_VER')) {
+    define('DH_PLUGIN_VER', '1.0.0');
+}
+
+if (!defined('PLUGIN_NAME')) {
+    define('PLUGIN_NAME', 'DEV_HELPER_PLUGIN');
+}
+
+if (!defined('PLUGIN_DOMAIN')) {
+    define('PLUGIN_DOMAIN', 'DHP');
+}
 
 register_activation_hook(
     __FILE__,
@@ -46,12 +54,14 @@ register_uninstall_hook(
     'pluginprefix_function_to_run'
 );
 
-function pluginprefix_function_to_run() {
+function pluginprefix_function_to_run()
+{
 
 }
 
-if( ! class_exists('PLUGIN_CLASS') ) {
-    class PLUGIN_CLASS {
+if (!class_exists('MAIN_PLUGIN_CLASS')) {
+    class MAIN_PLUGIN_CLASS
+    {
         /** @var string The plugin version number */
         var $version = '5.7.7';
 
@@ -65,7 +75,72 @@ if( ! class_exists('PLUGIN_CLASS') ) {
         var $instances = array();
 
 
+        /**
+         * This is our constructor
+         *
+         * @return void
+         */
+        private function __construct()
+        {
+            // back end
+            add_action('plugins_loaded', array($this, 'textdomain'));
+            add_action('admin_enqueue_scripts', array($this, 'admin_scripts'));
+            add_action('do_meta_boxes', array($this, 'create_metaboxes'), 10, 2);
+            add_action('save_post', array($this, 'save_custom_meta'), 1);
+
+            // front end
+            add_action('wp_enqueue_scripts', array($this, 'front_scripts'), 10);
+            add_filter('comment_form_defaults', array($this, 'custom_notes_filter'));
+        }
+
+        /**
+         * If an instance exists, this returns it.  If not, it creates one and
+         * retuns it.
+         *
+         * @return MAIN_PLUGIN_CLASS_Notes
+         */
+
+        public static function getInstance() {
+            if ( !self::$instance )
+                self::$instance = new self;
+            return self::$instance;
+        }
+
+        /**
+         * load textdomain
+         *
+         * @return void
+         */
+
+        public function textdomain() {
+
+            load_plugin_textdomain( PLUGIN_DOMAIN, false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+
+        }
+
+        /**
+         * Admin styles
+         *
+         * @return void
+         */
+
+        public function admin_scripts() {
+
+            $types = $this->get_post_types();
+
+            $screen	= get_current_screen();
+
+            if ( in_array( $screen->post_type , $types ) ) :
+
+                wp_enqueue_style( 'wpcmn-admin', plugins_url('lib/css/admin.css', __FILE__), array(), WPCMN_VER, 'all' );
+
+            endif;
+
+        }
+
 
     }
-
 }
+
+// Instantiate our class
+$MAIN_PLUGIN_OBJECT = MAIN_PLUGIN_CLASS::getInstance();
